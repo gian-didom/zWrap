1e8c67d63b1c4ef27120eabbc66484ed7e1f9e01
Gianfranco Di Domenico <gianfry@MBPdiGianfranco.home-life.hub>
2023-10-26 00:06:02 +0200

    GIT keeps deleting the BSP. Re-added BSP

--- wrapper/temp/getSizeAndOffsetsGDB.cpp
+++ wrapper/temp/getSizeAndOffsetsGDB.cpp
@@ -1,47 +1,27 @@
-#include "Subsystem1.h"
+#include "predict_01_Segmentation_types.h"
+
+
+#include "predict_01_Segmentation.h"
 
 
 
-#ifndef RTWTYPES_H
-#include "rtwtypes.h"
+#ifndef RTWTYPES_H#include "rtwtypes.h"
+
 
 #endif
 
 
-namespace coder {
-struct inputStructure {real_T _rtU_time;
+namespace coder {struct inputStructure {
+uint8_T _input1[16384];
 
-configuration_bus_architecture _rtU_Configurationbus;
-
-uint8_T _rtU_img[1048576];
-real_T _rtU_dir_Sun2Cam_Cam[3];
-
-real_T _rtU_DCM_Cam2PrincipalMoon[9];
-
-real_T _rtU_estimated_quaternion_J20002Body[4];
-real_T _rtU_earth_position_J2000[3];
-
-real_T _rtU_moon_position_J2000[3];
-
-real_T _rtU_sun_position_J2000[3];
 };
-
 inputStructure FULL_INPUT_STRUCT;
 
-struct {real_T _rtY_r_IP[3];
-
-real_T _rtY_Cov_r_IP[9];
-
-boolean_T _rtY_validity_IP;
-real_T _rtY_xHat[12];
-
-real_T _rtY_PHat[144];
-
-real_T _rtY_xProp[12];
-real_T _rtY_propagated_state_covariance[144];
-
+struct {
+dlarray _output;
 } FULL_OUTPUT_STRUCT;
 
 
 
-int main() {return 0;};}
+int main() {return 0;};
+}
