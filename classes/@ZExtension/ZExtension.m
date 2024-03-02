a260f0f0d4069f2e7d7e2dc0e16bb25a45eeb2c9
Gianfranco Di Domenico <gian.didom@gmail.com>
2024-03-02 16:39:58 +0100

    Set io and codegen methods as abstract in base class ZExtension

--- classes/@ZExtension/ZExtension.m
+++ classes/@ZExtension/ZExtension.m
@@ -1,25 +1,22 @@
 classdef ZExtension
-
+    
     properties
         board ZBoard;
     end
-
+    
     properties (Access = private)
         name
     end
-
+    
     methods
         function obj = ZExtension(board)
             obj.board = board;
         end
-
-        function io(obj)
-            % Implementation of extension during I/O definition phase
-        end
-
-        function codegen(obj)
-            % Implementation of extension during codegen phase
-        end
     end
-
+    
+    methods(Abstract)
+        % Implementation of extension during codegen phase
+        io(obj);
+        codegen(obj);
+    end
 end
