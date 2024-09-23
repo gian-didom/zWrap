2d5ffbb08de85a7a56636b923e1e6a187b094935
Henrik Harms <henrik.harms@dlr.de>
2024-09-23 12:13:14 +0200

    updated zWrap_call

--- wrapper/zWrap_call.m
+++ wrapper/zWrap_call.m
@@ -1,11 +1,12 @@
 
 codegen_path = 'C:\Users\EXAMPLE_USER\zWrap\coder_build_example\codegen\lib\example_entry_function';
 
-make_path = 'C:\Xilinx\Vitis\2023.2\gnuwin\bin';
+% custom_make_path = 'C:\Xilinx\Vitis\2023.2\gnuwin\bin';
 
 stack = '41943040'; %'819200';
 heap = '819200';
 
 %% Calling zWrap
-call_command = ['zWrap -path ', codegen_path, ' -ip 192.168.1.10 -udpport 7  -tcpport 8 -stack ', stack, ' -heap ', heap, ' -customMakePath ',  make_path];
+% call_command = ['zWrap -path ', codegen_path, ' -ip 192.168.1.10 -udpport 7  -tcpport 8 -stack ', stack, ' -heap ', heap, ' -customMakePath ',  custom_make_path];
+call_command = ['zWrap -path ', codegen_path, ' -ip 192.168.1.10 -udpport 7  -tcpport 8 -stack ', stack, ' -heap ', heap];
 eval(call_command)
